0da1acf3435559bc8c1f3a84225d1fa9485b2ca7
Karoly VEGH <karoly.vegh@gmail.com>
2021-01-04 21:39:41 +0100

    adding git install and git repo clone to prep script

--- scripts/step0_platformprep.sh
+++ scripts/step0_platformprep.sh
@@ -3,7 +3,7 @@
 
 #Choose the name for your ansible user
 ANSIBLE_USER="ansible"
-
+NUCLAB_REPO="https://github.com/mariuslapagna/nuclab"
 
 echo ""
 echo -e "######################"
@@ -45,7 +45,7 @@ fi
 # Ensuring the right repository is available 
 echo "Checking if the ansible repo is enabled" 
 if ! `subscription-manager repos --list-enabled | grep ansible-2.9-for-rhel-8-x86_64-rpms >/dev/null 2>&1` 
-  then
+  /then
     echo "...it isn't. Trying to enable..."
     if ! `/usr/bin/subscription-manager repos --enable=ansible-2.9-for-rhel-8-x86_64-rpms >/dev/null 2>&1`
       then
@@ -114,6 +114,35 @@ if ! `id $ANSIBLE_USER >/dev/null 2>&1`
     fi
   else
     echo -e "...Ansible user is already existing \n"
+fi
+
+#######################################################################
+# Installing git
+echo "Checking if git is installed"
+if ! `rpm -q git >/dev/null 2>&1`
+  then
+    echo "...it isn't, installing..." 
+    if ! `dnf install -y -q git`
+      then
+        echo "Installing git failed, exiting" 
+        exit 1
+      else 
+	echo -e "...done \n"
+    fi
+  else
+    echo -e "...git is already installed. \n"
+fi
+
+#######################################################################
+# creating the ansible user
+echo "Cloning the nuclab repository" 
+if ! `rm -rf /home/$ANSIBLE_USER/nuclab && sudo -i -u "$ANSIBLE_USER" git clone "$NUCLAB_REPO"` 
+  then 
+    echo "Cloning the repo failed, pls check." 
+    echo "exiting." 
+    exit 1
+  else
+    echo -e "...done, cloned to /home/$ANSIBLE_USER/nuclab ." 
 fi
 
 #######################################################################
